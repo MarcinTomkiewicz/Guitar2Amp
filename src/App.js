f9235751ad055ba08593f2b64e17b232f61f466d
MarcinTomkiewicz <marc.tomk@gmail.com>
2022-02-06 11:56:12 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Zoom from 'react-img-zoom'
 import { TopPanel } from "./components/topbar/TopPanel";
 
 function App() {
   return (
+    <>
   <TopPanel />
+    <div style={{width: "100%", height: "100%", background: "black"}}>
+  <div><img src={process.env.PUBLIC_URL + '/guitar2.jpg'} alt="guitar banner" /></div>
+  <Zoom
+  img={process.env.PUBLIC_URL + '/guitar2.jpg'} 
+  zoomScale={3}
+  width={1120}
+  height={396}
+/>
+  </div>
+
+  </>
   );
 }
 
